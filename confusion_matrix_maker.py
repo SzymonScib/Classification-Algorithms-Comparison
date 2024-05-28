b4bdab6aeed8422a6037eeb70250b7b02d9ece9c
SzymonScib <szymonscibcs@gmail.com>
2024-05-28 21:12:26 +0200

    modified:   confusion_matrix_maker.py

--- confusion_matrix_maker.py
+++ confusion_matrix_maker.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_confusion_matrix
 
 def make_matrix(TP,TN, FP, FN):
-    multiclass = np.array([[TP, FP],
-                           [FN, TN]]
+    multiclass = np.array([[TP, FN],
+                           [FP, TN]]
                             )
 
-    class_names = ['Malignant', 'Beningn']
+    class_names = ['M', 'B']
 
     fig, ax = plot_confusion_matrix(conf_mat=multiclass,
                                     colorbar=True,
-                                    show_absolute=False,
-                                    show_normed=True,
+                                    show_absolute=True,
+                                    show_normed=False,
                                     class_names=class_names)
     plt.show()
+
+make_matrix(50, 87, 2, 4)
